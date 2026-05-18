559c2ad601bcb5a0b2f6cdc43f34551befa494cb
taresh2504 <taresh25202@gmail.com>
2026-05-18 22:30:03 +0530

    mysql client installed and setting up of tables

--- Backend/project/project/settings.py
+++ Backend/project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'One_Night_Stay',
+        'USER': 'root',
+        'PASSWORD':'Tandy@123',
+        'PORT':3306
     }
 }
 
