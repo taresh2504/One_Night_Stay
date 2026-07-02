177fa4be580cc6f3f9c533e881dae44af23fee52
taresh2504 <taresh25202@gmail.com>
2026-07-02 14:07:33 +0530

    updated

--- Backend/project/app/admin.py
+++ Backend/project/app/admin.py
@@ -3,3 +3,11 @@ from .models import *
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(SubscriptionPlan)
+admin.site.register(UserSubscription)
+admin.site.register(Property)
+admin.site.register(PropertyImage)
+admin.site.register(Booking)
+admin.site.register(Payment)
+admin.site.register(Wishlist)
+admin.site.register(Review)
